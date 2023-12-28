07acb11dd2a741066f8e8057e5af9ce908d34b3c
richard-donbosco <ricdon41@gmail.com>
2023-12-28 12:08:50 -0500

    Add conform plugin and autocmd for autofmt

--- init.lua
+++ init.lua
@@ -650,5 +650,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end
 })
 
+-- Add autocmd for conform
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
+
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
