e0b7b7aafe85269ad8501d865b20f411a4a10dfc
richard-donbosco <ricdon41@gmail.com>
2023-12-28 12:10:24 -0500

    Add custom keymapfile and map for window resize

--- init.lua
+++ init.lua
@@ -48,7 +48,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -73,7 +73,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -625,6 +625,9 @@ cmp.setup {
     { name = 'path' },
   },
 }
+
+-- Add custom keysets
+require("richard.keyset")
 
 -- Add autocmd for gopls
 vim.api.nvim_create_autocmd("BufWritePre", {
---
+++ lua/richard/keyset.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
