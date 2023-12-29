33080d07507f2894886925753630206fa9ebcecc
richard-donbosco <ricdon41@gmail.com>
2023-12-28 19:56:59 -0500

    Add debug DAP go

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -53,15 +53,15 @@ return {
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          pause = '  ',
+          play = '  ',
+          step_into = '',
+          step_over = '  ',
+          step_out = '  ',
+          step_back = '  ',
+          run_last = '  ',
+          terminate = '  ',
+          disconnect = '  ',
         },
       },
     }
@@ -74,6 +74,57 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup()
+    require('dap-go').setup {
+      -- Additional dap configurations can be added.
+      -- dap_configurations accepts a list of tables where each entry
+      -- represents a dap configuration. For more details do:
+      -- :help dap-configuration
+      dap_configurations = {
+        -- {
+        --   type = "go",
+        --   name = "Debug",
+        --   request = "launch",
+        --   program = "${file}"
+        -- },
+        -- {
+        --   type = "go",
+        --   name = "Debug test", -- configuration for debugging test files
+        --   request = "launch",
+        --   mode = "test",
+        --   program = "${file}"
+        -- },
+        -- -- works with go.mod packages and sub packages
+        -- {
+        --   type = "go",
+        --   name = "Debug test (go.mod)",
+        --   request = "launch",
+        --   mode = "test",
+        --   program = "./${relativeFileDirname}"
+        -- }
+      },
+      -- delve configurations
+      delve = {
+        -- the path to the executable dlv which will be used for debugging.
+        -- by default, this is the "dlv" executable on your PATH.
+        path = "dlv",
+        -- time to wait for delve to initialize the debug session.
+        -- default to 20 seconds
+        initialize_timeout_sec = 20,
+        -- a string that defines the port to start delve debugger.
+        -- default to string "${port}" which instructs nvim-dap
+        -- to start the process in a random available port
+        port = "${port}",
+
+        -- additional args to pass to dlv
+        args = {}, -- 'dap', '-l', '127.0.0.1:${port}' },
+        -- the build flags that are passed to delve.
+        -- defaults to empty string, but can be used to provide flags
+        -- such as "-tags=unit" to make sure the test suite is
+        -- compiled during debugging, for example.
+        -- passing build flags using args is ineffective, as those are
+        -- ignored by delve in dap mode.
+        build_flags = "",
+      },
+    }
   end,
 }
