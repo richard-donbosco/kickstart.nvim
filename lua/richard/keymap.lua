477917fdf63305ac2fd99d26e072b4f7cab477ed
Richard Donbosco <rdonbosco@apple.com>
2025-07-05 21:21:30 -0500

    Fix keymap for error and diagnostic render

--- lua/richard/keymap.lua
+++ lua/richard/keymap.lua
@@ -105,3 +105,155 @@ wk.add {
   { '<leader>rcC', crates.open_crates_io, desc = 'Open crates.io', mode = 'n' },
   { '<leader>rcL', crates.open_lib_rs, desc = 'Open lib.rs', mode = 'n' },
 }
+
+-------------------
+--- Rustaceanvim mappings
+------------------
+wk.add {
+  -- Main rustaceanvim group
+  { '<leader>rs', group = 'rustaceanvim' },
+
+  -- LSP Client Management (RustAnalyzer commands)
+  { '<leader>rsl', group = 'lsp client' },
+  { '<leader>rsls', '<cmd>RustAnalyzer start<cr>', desc = 'Start LSP client', mode = 'n' },
+  { '<leader>rslS', '<cmd>RustAnalyzer stop<cr>', desc = 'Stop LSP client', mode = 'n' },
+  { '<leader>rslr', '<cmd>RustAnalyzer restart<cr>', desc = 'Restart LSP client', mode = 'n' },
+  { '<leader>rslR', '<cmd>RustAnalyzer reloadSettings<cr>', desc = 'Reload LSP settings', mode = 'n' },
+  {
+    '<leader>rslt',
+    function()
+      local target = vim.fn.input 'Target architecture (empty for default): '
+      vim.cmd('RustAnalyzer target ' .. target)
+    end,
+    desc = 'Set target architecture',
+    mode = 'n',
+  },
+
+  -- Run commands
+  { '<leader>rsR', group = 'run' },
+  { '<leader>rsRr', '<cmd>RustLsp runnables<cr>', desc = 'Run runnables', mode = 'n' },
+  { '<leader>rsRl', '<cmd>RustLsp!<cr>', desc = 'Run last runnable', mode = 'n' },
+  { '<leader>rsRc', '<cmd>RustLsp run<cr>', desc = 'Run at cursor', mode = 'n' },
+  { '<leader>rsRt', '<cmd>RustLsp testables<cr>', desc = 'Run testables', mode = 'n' },
+
+  -- Debug commands
+  { '<leader>rsd', group = 'debug' },
+  { '<leader>rsdd', '<cmd>RustLsp debuggables<cr>', desc = 'Debug debuggables', mode = 'n' },
+  { '<leader>rsdl', '<cmd>RustLsp! debuggables<cr>', desc = 'Debug last debuggable', mode = 'n' },
+  { '<leader>rsdc', '<cmd>RustLsp debug<cr>', desc = 'Debug at cursor', mode = 'n' },
+
+  -- Code actions and refactoring
+  { '<leader>rsa', group = 'actions' },
+  { '<leader>rsaa', '<cmd>RustLsp codeAction<cr>', desc = 'Code actions', mode = 'n' },
+  { '<leader>rsah', '<cmd>RustLsp hover actions<cr>', desc = 'Hover actions', mode = 'n' },
+  { '<leader>rsar', '<cmd>RustLsp hover range<cr>', desc = 'Hover range', mode = 'v' },
+  { '<leader>rsam', '<cmd>RustLsp expandMacro<cr>', desc = 'Expand macro', mode = 'n' },
+  { '<leader>rsau', '<cmd>RustLsp moveItem up<cr>', desc = 'Move item up', mode = 'n' },
+  { '<leader>rsad', '<cmd>RustLsp moveItem down<cr>', desc = 'Move item down', mode = 'n' },
+  { '<leader>rsaj', '<cmd>RustLsp joinLines<cr>', desc = 'Join lines', mode = 'n' },
+  {
+    '<leader>rsas',
+    function()
+      local query = vim.fn.input 'SSR query: '
+      vim.cmd('RustLsp ssr ' .. query)
+    end,
+    desc = 'Structural search replace',
+    mode = 'n',
+  },
+
+  -- Diagnostics with cycling support
+  {
+    '<leader>rse',
+    function()
+      -- Explain error and set up temporary cycling keymaps
+      vim.cmd 'RustLsp explainError'
+
+      -- Set up temporary keymaps for cycling
+      local opts = { buffer = true, silent = true }
+      vim.keymap.set('n', '>', '<cmd>RustLsp explainError cycle<cr>', opts)
+      vim.keymap.set('n', '<', '<cmd>RustLsp explainError cycle_prev<cr>', opts)
+      vim.keymap.set('n', '<Esc>', function()
+        -- Clear the temporary keymaps when Esc is pressed
+        pcall(vim.keymap.del, 'n', '>', { buffer = true })
+        pcall(vim.keymap.del, 'n', '<', { buffer = true })
+        pcall(vim.keymap.del, 'n', '<Esc>', { buffer = true })
+        -- Clear any lingering messages
+        vim.cmd 'echo ""'
+        vim.notify('Error explanation mode exited', vim.log.levels.INFO, { timeout = 1000 })
+      end, opts)
+
+      -- Show message about available keys with shorter timeout
+      vim.notify('Error explanation active. Use > (next), < (prev), Esc (exit)', vim.log.levels.INFO, { timeout = 3000 })
+    end,
+    desc = 'Explain error (then > next, < prev)',
+    mode = 'n',
+  },
+
+  {
+    '<leader>rsr',
+    function()
+      -- Render diagnostic and set up temporary cycling keymaps
+      vim.cmd 'RustLsp renderDiagnostic'
+
+      -- Set up temporary keymaps for cycling
+      local opts = { buffer = true, silent = true }
+      vim.keymap.set('n', '>', '<cmd>RustLsp renderDiagnostic cycle<cr>', opts)
+      vim.keymap.set('n', '<', '<cmd>RustLsp renderDiagnostic cycle_prev<cr>', opts)
+      vim.keymap.set('n', '<Esc>', function()
+        -- Clear the temporary keymaps when Esc is pressed
+        pcall(vim.keymap.del, 'n', '>', { buffer = true })
+        pcall(vim.keymap.del, 'n', '<', { buffer = true })
+        pcall(vim.keymap.del, 'n', '<Esc>', { buffer = true })
+        -- Clear any lingering messages
+        vim.cmd 'echo ""'
+        vim.notify('Diagnostic rendering mode exited', vim.log.levels.INFO, { timeout = 1000 })
+      end, opts)
+
+      -- Show message about available keys with shorter timeout
+      vim.notify('Diagnostic rendering active. Use > (next), < (prev), Esc (exit)', vim.log.levels.INFO, { timeout = 3000 })
+    end,
+    desc = 'Render diagnostic (then > next, < prev)',
+    mode = 'n',
+  },
+
+  -- Individual diagnostic commands (Error group)
+  { '<leader>rsEl', '<cmd>RustLsp relatedDiagnostics<cr>', desc = 'Related diagnostics', mode = 'n' },
+
+  -- Navigation
+  { '<leader>rsn', group = 'navigation' },
+  { '<leader>rsnc', '<cmd>RustLsp openCargo<cr>', desc = 'Open Cargo.toml', mode = 'n' },
+  { '<leader>rsnd', '<cmd>RustLsp openDocs<cr>', desc = 'Open docs.rs', mode = 'n' },
+  { '<leader>rsnp', '<cmd>RustLsp parentModule<cr>', desc = 'Parent module', mode = 'n' },
+  { '<leader>rsns', '<cmd>RustLsp workspaceSymbol<cr>', desc = 'Workspace symbols', mode = 'n' },
+  { '<leader>rsnS', '<cmd>RustLsp workspaceSymbol onlyTypes<cr>', desc = 'Workspace symbols (types)', mode = 'n' },
+  { '<leader>rsna', '<cmd>RustLsp workspaceSymbol allSymbols<cr>', desc = 'Workspace symbols (all)', mode = 'n' },
+
+  -- View commands
+  { '<leader>rsv', group = 'view' },
+  { '<leader>rsvs', '<cmd>RustLsp syntaxTree<cr>', desc = 'Syntax tree', mode = 'n' },
+  { '<leader>rsvm', '<cmd>RustLsp view mir<cr>', desc = 'View MIR', mode = 'n' },
+  { '<leader>rsvh', '<cmd>RustLsp view hir<cr>', desc = 'View HIR', mode = 'n' },
+  { '<leader>rsvg', '<cmd>RustLsp crateGraph svg crategraph.svg<cr>', desc = 'Crate graph', mode = 'n' },
+
+  -- Fly check
+  { '<leader>rsf', group = 'flycheck' },
+  { '<leader>rsfr', '<cmd>RustLsp flyCheck run<cr>', desc = 'Run flycheck', mode = 'n' },
+  { '<leader>rsfc', '<cmd>RustLsp flyCheck clear<cr>', desc = 'Clear flycheck', mode = 'n' },
+  { '<leader>rsfx', '<cmd>RustLsp flyCheck cancel<cr>', desc = 'Cancel flycheck', mode = 'n' },
+
+  -- Utilities
+  { '<leader>rsu', group = 'utilities' },
+  { '<leader>rsul', '<cmd>RustLsp logFile<cr>', desc = 'Open log file', mode = 'n' },
+
+  -- Rustc commands
+  { '<leader>rsc', group = 'rustc' },
+  {
+    '<leader>rscu',
+    function()
+      local args = vim.fn.input 'Unpretty args: '
+      vim.cmd('Rustc unpretty ' .. args)
+    end,
+    desc = 'Rustc unpretty',
+    mode = 'n',
+  },
+}
