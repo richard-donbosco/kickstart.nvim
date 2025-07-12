7506ecbc58c6c8bb357181f798de5e9ba414f9e2
Richard Donbosco <rdonbosco@apple.com>
2025-07-12 18:44:09 -0500

    Disable hlsearch

--- init.lua
+++ init.lua
@@ -108,6 +108,7 @@ vim.o.confirm = true
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
+vim.o.hlsearch = false
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
